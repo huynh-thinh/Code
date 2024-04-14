30dd889b7ca0c2209ca56ed56e3643d76bdeb5a7
huynh-thinh <thinh230207@gmail.com>
2024-04-14 11:12:30 +0700

    Update code

---
+++ lesson 1/index.js
@@ -0,0 +1,22 @@
+var fullname="Abc"
+var age =17
+
+var buttonclickme = document.getElementById('button')
+
+
+function onclick() {
+    alert('Click')
+}
+
+
+buttonclickme.addEventListener("click",onclick)
+
+// array
+var list=['a','b','c']
+console.log(list[0]);
+console.log(list.length);
+
+console.log(age + fullname)
+
+
+
---
+++ lesson 2/index1.js
@@ -0,0 +1,5 @@
+function sum (a){
+    var result = a + 10;
+    return result;
+} 
+console.log(sum(10) + 20);
